e3a3bc8d7868b935f7ad29aadf91b28ec2e4d360
Simon Oh <simon.oh@hey.com>
2024-09-05 00:59:33 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,21 @@
-import json
-from flask import Flask, request
+# import json
+# from flask import Flask, request
 
-import sites_of_grace
+# import sites_of_grace
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def getPath():
-    sites_of_grace.createMap("sites.json")
-    path = sites_of_grace.findPath("GP-GP", "B-TDB")
-    return path
+# @app.route("/", methods=['GET'])
+# def getPath():
+#     sites_of_grace.createMap("sites.json")
+#     path = sites_of_grace.findPath("GP-GP", "B-TDB")
+#     return path
 
-@app.route("/", methods=['POST'])
-def postPath():
-    return "post"
+# @app.route("/", methods=['POST'])
+# def postPath():
+#     return "post"
 
-if __name__ == '__main__':
-   app.run()
+# if __name__ == '__main__':
+#    app.run()
+
+print("hello")
