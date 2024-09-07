09677a7cec60c71960adda35e6ae085a144126f6
Simon Oh <simon.oh@hey.com>
2024-09-06 21:39:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,31 @@
-import json
-from flask import Flask, request
+# import json
+# from flask import Flask, request
 
-import sites_of_grace
+# import sites_of_grace
+
+# app = Flask(__name__)
+
+# # @app.route("/", methods=['GET'])
+# # def getPath():
+# #     sites_of_grace.createMap("sites.json")
+# #     path = sites_of_grace.findPath("GP-GP", "B-TDB")
+# #     return path
+
+# # @app.route("/", methods=['POST'])
+# # def postPath():
+# #     return "post"
+
+# if __name__ == '__main__':
+#    app.run()
+
+from flask import Flask
 
 app = Flask(__name__)
 
-# @app.route("/", methods=['GET'])
-# def getPath():
-#     sites_of_grace.createMap("sites.json")
-#     path = sites_of_grace.findPath("GP-GP", "B-TDB")
-#     return path
-
-# @app.route("/", methods=['POST'])
-# def postPath():
-#     return "post"
+@app.route('/')
+def hello():
+    return "Hello, World!"
 
 if __name__ == '__main__':
-   app.run()
+    print("hello")  # This explains the "hello" in the logs
+    app.run()
