b2d3d72c919421e06cd734d07d70c2e47df5508a
Simon Oh <simon.oh@hey.com>
2024-10-22 23:20:49 -0700

    Starting react stuff

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ import sites_of_grace
 
 app = Flask(__name__)
 
-client = MongoClient("localhost", 27017) #create a client instance of MongoDB
-db = client.flask_db #create (or read when it doesn't exist) a database within the client instance called flask_db
-locations = db.locations #create (or read when it doesn't exist) a collection called locations. Collection stores data called documents
+######## MongoDB stuff
+client = MongoClient("localhost", 27017) #create an instance of MongoDB named client
+db = client.flask_db #read (or create when it doesn't exist) a database within the client instance called flask_db
+locations = db.locations #read (or create when it doesn't exist) a collection called locations. Collection stores data called documents
+########
 
 #set up logging
 logging.basicConfig(level=logging.DEBUG)
@@ -37,7 +39,7 @@ def getPath():
         sourceID = request.args.get("source", "")
         sourceIDUpper = sourceID.upper()
         #path = sites_of_grace.findPath("Gravesite Plain", "Three-Path Cross")
-        print("----------------", destinationIDUpper, "     ", sourceIDUpper)
+        #print("----------------", destinationIDUpper, "     ", sourceIDUpper)
         pathDict = sites_of_grace.findPath(sourceIDUpper, destinationIDUpper)
         return render_template("results.html", source = sites_of_grace.map.sitesOfGracesFullName, destination = sites_of_grace.map.sitesOfGracesFullName, path = pathDict["pathName"])
     except Exception as e:
